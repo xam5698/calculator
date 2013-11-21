ddacf5fdcc50a8d21b001ab369b24850e7b23a0b
Joshua Beitler <joshbeitler@gmail.com>
2013-11-21 10:28:43 -0700

    doit

--- test.py
+++ test.py
@@ -3,15 +3,9 @@
 # Travis Adair
 # Joseph Drake
 # Xylonah Stott
-<<<<<<< HEAD
-<<<<<<< HEAD
 # James Condie 
-=======
 #Logan Hirschi
 # Ryan Barlow
->>>>>>> a66f9155901fdefe35f8fa0421eb849e59d3255b
-=======
 # Logan Hirschi
 # Ryan Barlow
 # Max McCarroll 
->>>>>>> 52b3488c5abbc15c0c08703b293957a3be83c505
