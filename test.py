f68ff42d0ce1b2d64e545aa2b92b69248ffd9422
Joshua Beitler <joshbeitler@gmail.com>
2013-11-25 10:16:26 -0700

    Update test.py

--- test.py
+++ test.py
@@ -3,14 +3,9 @@
 # Travis Adair
 # Joseph Drake
 # Xylonah Stott
-<<<<<<< HEAD
-<<<<<<< HEAD
 # Taylor Taigen
-=======
-#Logan Hirschi
+# Logan Hirschi
 # Ryan Barlow
->>>>>>> a66f9155901fdefe35f8fa0421eb849e59d3255b
-=======
 # James Condie 
 # Logan Hirschi
 # Ryan Barlow
@@ -18,4 +13,3 @@
 # Christian Loredo
 # Smitty Werbenmenjensen (Deyge)
 # Jonathan page
->>>>>>> 6e36afb1f2492fa81ff9518b3b79f2eaacd36a91
